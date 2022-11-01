e81819f8e79e588b1bc5428e939fd5d94f2d6f7d
yellowflash2041 <yellowflash2041@gmail.com>
2022-11-01 17:18:56 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// Do NOT edit nor remove any existing code when testing or submitting
+const mainFunction = (input) => {
+  // Place your code here
+  return input.split('').sort().reverse().join('');
+}
+  
